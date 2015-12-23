5554f980c1cb9cf110f658a6a476642310754dd4
Kayla Smith <kytrick@gmail.com>
2015-12-23 02:05:28 -0800

    defended against sneakiness by escaping data, also separated out data retrieval pipeline for better exception handling later

--- tagony.py
+++ tagony.py
@@ -6,6 +6,9 @@ from htmlentitydefs import name2codepoint
 from HTMLParser import HTMLParser
 import json
 import requests
+from markupsafe import escape
+from cgi import escape # https://wiki.python.org/moin/EscapingHtml
+import re
 
 
 # create a subclass and override the handler methods
@@ -28,9 +31,11 @@ class MyHTMLParser(HTMLParser):
         # (re)build the string of attrs that would appear inside a start tag.
         attr_string = ''
         for attr in attrs:
+            # conditional protects against trying to cast None as unicode
+            if attr[1] is None:
+                attr = (attr[0], '')
             attr_string += ' ' + attr[0] + '="' + attr[1] + '"'
 
-           # attr_string += ' %s="%s"' % (attr[0], attr[1])
 
         # generate start tag string that includes attrs.
         tempstring = '<span class="my_' + tag + '">&lt;' + tag + attr_string + '&gt;</span>'
@@ -53,6 +58,8 @@ class MyHTMLParser(HTMLParser):
         # (re)build the string of attrs that would appear inside a start tag.
         attr_string = ''
         for attr in attrs:
+            if attr[1] is None:
+                attr = (attr[0], '')
             attr_string = attr_string + ' ' + attr[0] + '=' + '"' + attr[1] + '"'
 
         # generate start tag string that includes attrs.
@@ -73,33 +80,60 @@ class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
         # parser doesn't work perfectly with <!------>stuff-->
         # print "Comment  :", data
-        tempstring = '<span class="my_comment">&lt;!--' + data + '--&gt;</span>'
+        tempstring = '<span class="my_comment">&lt;!--' + escape(data) + '--&gt;</span>'
         # print tempstring
         self.augmented_html += tempstring
 
     def handle_decl(self, data):
         # print "Decl     :", data
-        tempstring = '<span class="my_decl">' + '&lt;!' + data + '&gt;</span>'
+        tempstring = '<span class="my_decl">' + '&lt;!' + escape(data) + '&gt;</span>'
         # print tempstring
         self.augmented_html += tempstring
 
     def handle_data(self, data):
         # print "Encountered some data  :", data
-        tempstring = data
+        # need to escape
+        tempstring = escape(data)
         self.augmented_html += tempstring
 
     def handle_pi(self, data):
-        self.augmented_html += data
+        self.augmented_html += escape(data)
 
     def unknown_decl(self, decl):
-        self.augmented_html += decl
+        # need to escape
+        self.augmented_html += escape(decl)
 
 
 def get_parsed_data(input_url):
     parser = MyHTMLParser()
-    data = requests.get(input_url).text
+    #clean_url = clean_my_url(input_url)
+    #data = requests.get(clean_url).text
+    data = fetch_data(input_url)
     parser.feed(data)
     return parser
 
+def fetch_data(input_url):
+
+    clean_url = clean_my_url(input_url)
+    data = requests.get(clean_url).text
+    return data
+
+
+def clean_my_url(input_url):
+    ''' this function does some sanity checking on the input url before giving
+        it to get_parsed_data
+    '''
+
+    # did the user forget the http:// ?
+    if input_url.find('://') < 0:
+        input_url = 'http://' + input_url
+
+    # data = requests.get(input_url).text
+    return input_url
+    #return data
+
+
+
+
 # instantiate the parser and feed it some HTML
-parser = MyHTMLParser()
+#parser = MyHTMLParser()
